6d5d0815d63974350b94449f29e50c7d6984a2fe
manishshingre <shingre.manish11@gmail.com>
2021-11-14 20:24:51 +0530

    First commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val actionBar = supportActionBar
+        actionBar!!.title = "Registration Form"
+
+        loginBtn1.setOnClickListener{
+            val intent = Intent(this, RegistrationForm::class.java)
+            startActivity(intent)
+
+        }
+    }
+}
